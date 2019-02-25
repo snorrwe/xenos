b1054db5865e464f67fe00e3995d799f44c0052c
Daniel Kiss <littlesnorrboy@gmail.com>
2019-02-25 13:34:46 +0100

    Fix an issue causing workers to stand idly

--- src/creeps/repairer.rs
+++ src/creeps/repairer.rs
@@ -2,13 +2,11 @@
 //!
 use super::super::bt::*;
 use super::{builder, get_energy, upgrader};
-use screeps::{
-    game::get_object_erased,
-    objects::{Creep, Structure},
-    prelude::*,
-    ReturnCode,
+use screeps::{objects::Creep, prelude::*, ReturnCode};
+use stdweb::{
+    unstable::{TryFrom, TryInto},
+    Reference,
 };
-use stdweb::unstable::TryFrom;
 
 pub fn run<'a>(creep: &'a Creep) -> Task<'a> {
     trace!("Running repairer {}", creep.name());
@@ -37,51 +35,31 @@ pub fn attempt_repair<'a>(creep: &'a Creep) -> ExecutionResult {
         Err("empty".into())
     } else {
         trace!("Repairing");
-        let target = validate_creep_target_as_repair_target(creep)
-            .map(|target| {
-                creep.memory().set("target", target.id());
-                target
-            })
-            .or_else(|| find_repair_target(creep))
-            .ok_or_else(|| {
-                let error = format!("Could not find a repair target");
-                debug!("{}", error);
-                error
-            })?;
-        trace!("Got repair target {:?}", target.id());
+        let target = find_repair_target(creep).ok_or_else(|| {
+            let error = format!("Could not find a repair target");
+            debug!("{}", error);
+            error
+        })?;
         repair(creep, &target)
     }
 }
 
-fn validate_creep_target_as_repair_target<'a>(creep: &'a Creep) -> Option<Structure> {
-    let target = creep.memory().string("target").ok()??;
-    let target = get_object_erased(target.as_str())?;
-
-    let target = Structure::try_from(target.as_ref().clone())
-        .map_err(|e| {
-            debug!("failed to read exisitng target, {:?}", e);
-        })
-        .ok()?;
-
-    let repairable = target
-        .as_attackable()
-        .map(|s| s.hits() < s.hits_max())
-        .ok_or_else(|| {
-            error!("Expected target to be attackable {:?}", target.id());
-            creep.memory().del("target");
-        })
-        .ok()?;
-    if repairable {
-        Some(target)
-    } else {
-        None
-    }
+fn validate_creep_target_as_repair_target<'a>(creep: &'a Creep) -> Option<Reference> {
+    let id = creep.memory().string("target").ok()??;
+    let target = js! {
+        const target = @{&id};
+        return Game.getObjectById(target);
+    };
+    target
+        .try_into()
+        .map_err(|e| warn!("Failed to read repair target {} {:?}", id, e))
+        .ok()
 }
 
-fn repair<'a>(creep: &'a Creep, target: &'a Structure) -> ExecutionResult {
+fn repair<'a>(creep: &'a Creep, target: &'a Reference) -> ExecutionResult {
     let res = js! {
         const creep = @{creep};
-        let target = @{target.as_ref()};
+        let target = @{target};
         let result = creep.repair(target);
         if (result == ERR_NOT_IN_RANGE) {
             result = creep.moveTo(target);
@@ -100,7 +78,7 @@ fn repair<'a>(creep: &'a Creep, target: &'a Structure) -> ExecutionResult {
     }
 }
 
-fn find_repair_target<'a>(creep: &'a Creep) -> Option<Structure> {
+fn find_repair_target<'a>(creep: &'a Creep) -> Option<Reference> {
     trace!("Finding repair target");
 
     let room = creep.room();
@@ -112,5 +90,8 @@ fn find_repair_target<'a>(creep: &'a Creep) -> Option<Structure> {
         return candidates[0];
     };
 
-    Structure::try_from(result).ok()
+    Reference::try_from(result)
+        .map_err(|e| warn!("Failed to convert find repair target {:?}", e))
+        .ok()
 }
+
