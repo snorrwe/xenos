6375199b61159f38bea48b6ff6a18746aca93cce
Daniel Kiss <littlesnorrboy@gmail.com>
2019-08-17 10:38:07 +0200

    Make VERSION use semver

--- build.rs
+++ build.rs
@@ -1,8 +1,19 @@
-extern crate chrono;
+extern crate semver;
+
+use semver::Version;
 use std::fs;
 
 fn main() {
-    let now = chrono::Utc::now();
-    let now = format!("{}", now);
-    fs::write("VERSION", now).expect("Failed to create version file");
+    let mut version = if let Ok(version) = fs::read_to_string("VERSION") {
+        Version::parse(&version)
+            .or(Version::parse("0.1.0"))
+            .unwrap()
+    } else {
+        Version::parse("0.1.0").unwrap()
+    };
+
+    version.increment_minor();
+
+    fs::write("VERSION", format!("{}", version)).expect("Failed to create version file");
 }
+
